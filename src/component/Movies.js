9d2c7e994feb6a2eef3a05d5d933caf55282a94a
tahany777 <tahanymohammed777@hotmail.com>
2021-08-12 16:25:53 +0300

    nn

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -4,9 +4,9 @@ export class Movies extends Component {
   render() {
     return (
       <div>
-        <h1>{this.props.key}</h1>
-        <h2>{this.props.title}</h2>
-        <h3>{this.props.vote}</h3>
+        <h1>ID: {this.props.key}</h1>
+        <h2>Title: {this.props.title}</h2>
+        <h3>Votes: {this.props.vote}</h3>
         <img src={this.props.img} alt=""/>
       </div>
     )
